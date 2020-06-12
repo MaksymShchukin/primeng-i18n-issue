4b187fcce273bd3d6132633fd68e861fba5c02ff
Maksim Shchukin <maxim.shchukin@office.gconnect.ch>
2020-06-12 11:44:03 +0300

    added primeng added angular localization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { SelectItem } from 'primeng/api';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'primeng-i18n-issue';
+  public items: SelectItem[] = [{ label: 'Blue', value: 1 }, { label: 'Green', value: 2 }, { label: 'Red', value: 3 }];
 }
